b0f392a84567b6d1379c033108cfab2aca7ccdd6
Marc Minor <marceminor@gmail.com>
2014-11-15 21:10:19 -0800

    implemented password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { maximum: 256 },
 						format: { with: VALID_EMAIL_REGEX },
 						uniqueness: { case_sensitive: false }
+	validates :password, length: { minimum: 6 }
 	has_secure_password
 end
