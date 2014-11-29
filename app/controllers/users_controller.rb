7d9ba2b2c6d4ad37901c602784ac0ee2a6bfa2b0
Marc Minor <marceminor@gmail.com>
2014-11-29 14:54:00 -0800

    completed implementation of admin & delete functionality - up to 9.4.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: :destroy
 
   def index
+    @users = User.paginate(page: params[:page])
   end
   
   def create
@@ -40,6 +42,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
@@ -54,6 +59,7 @@ class UsersController < ApplicationController
     # confirms a logged-in user
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
@@ -64,4 +70,10 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+
+    # confirms an admin user
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
