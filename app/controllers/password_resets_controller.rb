7643e8286d7bfaa935bc03f11606e436feab28c6
Marc Minor <mminor@apple.com>
2014-12-10 17:54:07 -0800

    completed password resets & email config for production

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,    only: [:edit, :update]
   before_action :valid_user,  only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -21,7 +22,30 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if both_passwords_blank?
+      flash.now[:danger] = "Password/confirmation can't be blank"
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # Returns true if password & confirmation are blank.
+    def both_passwords_blank?
+      params[:user][:password].blank? &&
+      params[:user][:password_confirmation].blank?
+    end
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -32,6 +56,13 @@ class PasswordResetsController < ApplicationController
       unless (@user && @user.activated? &&
               @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
+      end
+    end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
       end
     end
 end
