f4d4473a7b8f67258004e380ef226a05fd6399a1
Marc Minor <mminor@apple.com>
2014-12-16 14:10:09 -0800

    completed up to 11.4 in chapter11

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,4 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-
 end
