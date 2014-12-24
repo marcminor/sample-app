f532fa4a777351a810b38cca9a522dbfcb50600d
Marc Minor <marceminor@gmail.com>
2014-12-24 13:41:09 -0800

    completed follow/unfollow tests - Up to 12.3

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -10,7 +10,7 @@ class RelationshipsControllerTest < ActionController::TestCase
 	end
 
 	test "destroy should require logged-in user" do 
-		assert_no difference 'Relationship.count' do 
+		assert_no_difference 'Relationship.count' do 
 			delete :destroy, id: relationships(:one)
 		end
 		assert_redirected_to login_url
