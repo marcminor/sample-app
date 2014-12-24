f8b2a83567991ceaeab5d50f1498d76172bdfe1e
Marc Minor <marceminor@gmail.com>
2014-12-24 14:12:23 -0800

    completed chapter 12

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,74 +12,92 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "name should be present" do
-    	@user.name = "     "
-    	assert_not @user.valid?
-  	end
+    @user.name = "     "
+    assert_not @user.valid?
+  end
 
-  	test "email should be present" do
-  		@user.email = "    "
-  		assert_not @user.valid?
-  	end
+	test "email should be present" do
+		@user.email = "    "
+		assert_not @user.valid?
+	end
 
-  	test "name should not be too long" do
-  		@user.name = "a" * 51
-  		assert_not @user.valid?
-  	end
+	test "name should not be too long" do
+		@user.name = "a" * 51
+		assert_not @user.valid?
+	end
 
-  	test "email should not be too long" do
-  		@user.email = "a" * 256
-  		assert_not @user.valid?
-  	end
+	test "email should not be too long" do
+		@user.email = "a" * 256
+		assert_not @user.valid?
+	end
 
-  	test "email validation should accept valid addresses" do
-  		valid_addresses = %w[name@name.com USER@name.com user2@NAME.COM A_Foo_bar@foo.bar.org
-  							first.last@foo.jp alice+bob@baz.cn]
-  		valid_addresses.each do |valid_address|
-  			@user.email = valid_address
-  			assert @user.valid?, "#{valid_address.inspect} should be valid"
-  		end
-  	end
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[name@name.com USER@name.com user2@NAME.COM A_Foo_bar@foo.bar.org
+							first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_address|
+			@user.email = valid_address
+			assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end
+	end
 
-  	test "email validation should reject invalid addresses" do
-	    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-	                           foo@bar_baz.com foo@bar+baz.com]
-	    invalid_addresses.each do |invalid_address|
-	      @user.email = invalid_address
-	      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-    	end
+	test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
   	end
+	end
 
-    test "email addresses should be unique" do
-	    duplicate_user = @user.dup
-	    duplicate_user.email = @user.email.upcase
-	    @user.save
-	    assert_not duplicate_user.valid?
-  	end
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+	end
 
-  	test "password should have a minimum length" do
-  		@user.password = @user.password_confirmation = "a" * 5
-  	end
+	test "password should have a minimum length" do
+		@user.password = @user.password_confirmation = "a" * 5
+	end
 
-    test "authenticated? should return false for a user with nil digest" do
-      assert_not @user.authenticated?(:remember, '')
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem Ipsum")
+    assert_difference 'Micropost.count', -1 do 
+      @user.destroy
     end
+  end
 
-    test "associated microposts should be destroyed" do
-      @user.save
-      @user.microposts.create!(content: "Lorem Ipsum")
-      assert_difference 'Micropost.count', -1 do 
-        @user.destroy
-      end
-    end
+  test "should follow and unfollow a user" do 
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 
-    test "should follow and unfollow a user" do 
-      michael = users(:michael)
-      archer = users(:archer)
-      assert_not michael.following?(archer)
-      michael.follow(archer)
-      assert michael.following?(archer)
-      assert archer.followers.include?(michael)
-      michael.unfollow(archer)
-      assert_not michael.following?(archer)
+  test "feed should have the right posts" do 
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    #Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
     end
+    #Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    #Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
